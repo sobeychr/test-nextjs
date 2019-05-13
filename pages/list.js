26671f99ad5f0b65a2ac2d6abb7c1d90e3bf4659
Christopher Sobey <christopher.sobey@mindgeek.com>
2019-05-13 14:31:48 -0400

    + Adding server content ! Incomplete

--- pages/list.js
+++ pages/list.js
@@ -4,7 +4,7 @@ import withLayout from './../components/Layout';
 
 const PostLink = props => (
     <li>
-        <Link href={`/post?title=${props.title}`}>
+        <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
             <a>{props.title}</a>
         </Link>
     </li>
@@ -13,9 +13,9 @@ const PostLink = props => (
 const Page = () => <div>
     <h1>LISTING</h1>
     <ul>
-        <PostLink title='hello 0' />
-        <PostLink title='hello 5' />
-        <PostLink title='asdf' />
+        <PostLink id='hello-0' title='hello 0' />
+        <PostLink id='hello-5' title='hello 5' />
+        <PostLink id='pinisse' title='asdf' />
     </ul>
 </div>;
 
